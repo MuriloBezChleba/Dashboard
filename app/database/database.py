03739ee154ecb3429e74dea6d4954ff122ac36a6
Murilo Bez Chleba <murilogbc2006@gmail.com>
2025-03-20 18:04:32 -0300

    first

---
+++ app/database/database.py
@@ -0,0 +1,11 @@
+from motor import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URL = os.getenv('MONGO_URL')
+
+client = AsyncIOMotorClient(MONGO_URL)
+
+db = client[dashboard]
